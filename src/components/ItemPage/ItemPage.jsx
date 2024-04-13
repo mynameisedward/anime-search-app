de81106b1641d57c0fa0b976e9f6a93b908cc62c
Edward <snegovikzimniy@gmail.com>
2024-04-13 14:51:45 +0300

    data inside item page

--- src/components/ItemPage/ItemPage.jsx
+++ src/components/ItemPage/ItemPage.jsx
@@ -3,63 +3,101 @@ import s from './ItemPage.module.css'
 import { useLocation, useParams } from 'react-router-dom'
 import animeImg from '../../assets/animeImage.webp'
 import axios from 'axios'
+import Preloader from '../Preloader/Preloader'
 
 
 const ItemPage = () => {
 
 
-    const [item, setItem] = useState()
-    
-    const {id} = useParams()
+    const [item, setItem] = useState({})
+    const [loading, setLoading] = useState(true)
+
+
+    const { id } = useParams()
 
     const location = useLocation()
     const currentUrl = location.pathname
 
     useEffect(() => {
-        // const fetchData = async () => {
-        //     if(currentUrl.includes('anime')) {
-        //         let response = await axios.get(`https://api.jikan.moe/v4/anime/${id}/full`);
-        //         setItem(response.data.data);
-        //     } else if(currentUrl.includes('manga')) {
-        //         let response = await axios.get(`https://api.jikan.moe/v4/manga/${id}/full`);
-        //         setItem(response.data.data);
-        //     }
-        // }
-        // fetchData()
+        const fetchData = async () => {
+            if (currentUrl.includes('anime')) {
+                let response = await axios.get(`https://api.jikan.moe/v4/anime/${id}/full`);
+                setItem(response.data.data);
+            } else if (currentUrl.includes('manga')) {
+                let response = await axios.get(`https://api.jikan.moe/v4/manga/${id}/full`);
+                setItem(response.data.data);
+            }
+        }
+
+
+
+        fetchData()
+        setTimeout(() => { // Добавляем времени чтобы люди успели рассмотреть этого чудесного Preloader-кота
+            setLoading(false)
+        }, 3000)
     }, [])
-    
+
     // includes
     window.id = id
     window.stateItem = item
 
+    // короче у аниме и манги разные данные и данных так же может не быть некоторых данных которые ожидаются
+
+
     return (
         <div>
             <div className={s.container}>
-                <div className={s.firstRow}>
-                    <div className={s.animeImageDiv}>
-                        {/* <img src={item.images.webp.large_image_url} alt="" className={s.animeImage} /> */}
-                        <img src={animeImg} alt="" className={s.animeImage} />
-                    </div>
-                    <div className={s.animeInfo}>
-                        <h1 className={s.animeEnglishTitle}>Hunter x Hunter</h1>
-                        <h2 className={s.animeJapaneseTitle}>HUNTER×HUNTER（ハンター×ハンター）</h2>
-                        <ul className={s.animeInfoList}>
-                            <li className={s.animeInfoItem}>Episodes: <span>{12}</span></li>
-                            <li className={s.animeInfoItem}>Source: <span>Manga</span></li>
-                            <li className={s.animeInfoItem}>Rating: <span>PG-13 - Teens 13 or older</span></li>
-                            <li className={s.animeInfoItem}>Score: <span>9.04</span></li>
-                            <li className={s.animeInfoItem}>Year: <span>2011</span></li>
-                            <li className={s.animeInfoItem}>Status: <span>Finished Airing</span></li>
-                            <li className={s.animeInfoItem}>Duration: <span>23 min per ep</span></li>
-                            <li className={s.animeInfoItem}>Type: <span>TV</span></li>
-                            
-                        </ul>
-                    </div>
-                </div>
-                <div className={s.synopsisDiv}>
-                    <h3 className={s.synopsisTitle}>Synopsis</h3>
-                    <p className={s.synopsisText}>Hunters devote themselves to accomplishing hazardous tasks, all from traversing the world's uncharted territories to locating rare items and monsters. Before becoming a Hunter, one must pass the Hunter Examination—a high-risk selection process in which most applicants end up handicapped or worse, deceased.\n\nAmbitious participants who challenge the notorious exam carry their own reason. What drives 12-year-old Gon Freecss is finding Ging, his father and a Hunter himself. Believing that he will meet his father by becoming a Hunter, Gon takes the first step to walk the same path.\n\nDuring the Hunter Examination, Gon befriends the medical student Leorio Paladiknight, the vindictive Kurapika, and ex-assassin Killua Zoldyck. While their motives vastly differ from each other, they band together for a common goal and begin to venture into a perilous world.\n\n[Written by MAL Rewrite]</p>
-                </div>
+                {
+                    loading ? 
+                    <Preloader />
+                    :
+                    <>
+                        
+                        <div className={s.firstRow}>
+                            <div className={s.animeImageDiv}>
+                                <img src={item.images.webp.large_image_url} alt="" className={s.animeImage} />
+                                {/* <img src={animeImg} alt="" className={s.animeImage} /> */}
+                            </div>
+                            <div className={s.animeInfo}>
+                                <h1 className={s.animeEnglishTitle}>{item.title_english}</h1>
+                                <h2 className={s.animeJapaneseTitle}>{item.title_japanese}</h2>
+                                <ul className={s.animeInfoList}>
+                                    {
+                                        currentUrl.includes('anime') 
+                                        &&
+                                        <>
+                                            <li className={s.animeInfoItem}>Episodes: <span>{item.episodes}</span></li>
+                                            <li className={s.animeInfoItem}>Source: <span>{item.source}</span></li>
+                                            <li className={s.animeInfoItem}>Rating: <span>{item.rating}</span></li>
+                                            <li className={s.animeInfoItem}>Genres: <span>{item.genres.map((genre) => <span className={s.genreName}>{genre.name}</span>)}</span></li>
+                                            <li className={s.animeInfoItem}>Score: <span>{item.score}</span></li>
+                                            <li className={s.animeInfoItem}>Year: <span>{item.year}</span></li>
+                                            <li className={s.animeInfoItem}>Status: <span>{item.status}</span></li>
+                                            <li className={s.animeInfoItem}>Duration: <span>{item.duration}</span></li>
+                                            <li className={s.animeInfoItem}>Type: <span>{item.type}</span></li>
+                                        </>
+                                    }
+                                    {
+                                        currentUrl.includes('manga') &&
+                                        <>
+                                            { item.chapters && <li className={s.animeInfoItem}>Chapters: <span>{item.chapters}</span></li>  }
+                                            <li className={s.animeInfoItem}>Score: <span>{item.score}</span></li>
+                                            <li className={s.animeInfoItem}>Genres: <span>{item.genres.map((genre) => <span className={s.genreName}>{genre.name}</span>)}</span></li>
+                                            <li className={s.animeInfoItem}>Rank: <span>{item.rank}</span></li>
+                                            <li className={s.animeInfoItem}>Type: <span>{item.type}</span></li>
+                                            <li className={s.animeInfoItem}>Status: <span>{item.status}</span></li>
+                                            <li className={s.animeInfoItem}>Members: <span>{item.members}</span></li>
+                                        </>
+                                    }
+                                </ul>
+                            </div>
+                        </div>
+                        <div className={s.synopsisDiv}>
+                            <h3 className={s.synopsisTitle}>Synopsis</h3>
+                            <p className={s.synopsisText}>{item.synopsis}</p>
+                        </div>
+                    </>
+                }
             </div>
         </div>
 

015355eb31afc92dc0ee2e37945ccb299cd05a2d
Edward <snegovikzimniy@gmail.com>
2024-05-17 16:47:34 +0300

    new com

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     return (
 
         <div className='App'>
-            <BrowserRouter basename="/anime-search-app">
+            <BrowserRouter>
                     <Header />
                     <Main />
                     <Footer />

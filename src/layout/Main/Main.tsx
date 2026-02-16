7c4dcc2743773c881eb9c78477913b69392700b1
Edward <snegovikzimniy@gmail.com>
2026-02-16 19:11:04 +0300

    feat: add Welcome page

--- src/layout/Main/Main.tsx
+++ src/layout/Main/Main.tsx
@@ -2,19 +2,13 @@ import s from './Main.module.css';
 import Content from '../../pages/Content/Content';
 import { Route, Routes } from 'react-router-dom';
 import ItemPage from '../../pages/ItemPage/ItemPage';
+import { WelcomePage } from '../../pages/WelcomePage/WelcomePage';
 
 const Main = () => {
   return (
     <main className={s.Main}>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <div className={s.container}>
-              <h1 className={s.title}>Выберите аниме или мангу!</h1>
-            </div>
-          }
-        />
+        <Route path="/" element={<WelcomePage />} />
         <Route path="/anime" element={<Content content="anime" />} />
         <Route path="/anime/:id" element={<ItemPage />} />
         <Route path="/manga" element={<Content content="manga" />} />
---
+++ src/pages/WelcomePage/WelcomePage.tsx
@@ -0,0 +1,9 @@
+import s from './WelcomePage.module.css';
+
+export const WelcomePage = () => {
+  return (
+    <div className={s.container}>
+      <h1 className={s.title}>Выберите аниме или мангу!</h1>
+    </div>
+  );
+};
